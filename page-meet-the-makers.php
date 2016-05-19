735718e2ea08ca21ecf9b1a07c2ec1ceb6ef4eab
David Beauchamp <dbeauchamp@makermedia.com>
2016-05-19 13:39:18 -0700

    Angular changes

--- page-meet-the-makers.php
+++ page-meet-the-makers.php
@@ -4,11 +4,6 @@
 */
 get_header(); ?>
 
-<script type="text/javascript" src="<?php echo get_bloginfo('template_directory');?>/js/owl.carousel.min.js"></script>
-<!-- script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script-->
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.js"></script>
-<script src="<?php echo get_bloginfo('template_directory');?>/angular/controller.js"></script>
-
 <div class="mtm" ng-app="mtm">
 
   <div class="container">
